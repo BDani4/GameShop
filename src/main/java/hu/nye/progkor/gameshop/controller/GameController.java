b3303f2144c0dd82bc50af6954bb6356d85a50ab
Béres Dani <beres.dani4@gmail.com>
2022-05-17 15:28:03 +0200

    Checkstyle

--- src/main/java/hu/nye/progkor/gameshop/controller/GameController.java
+++ src/main/java/hu/nye/progkor/gameshop/controller/GameController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * Game Controller.
+ * Game Controller Java doc comment.
  */
 @Controller
 @RequestMapping("/game")
