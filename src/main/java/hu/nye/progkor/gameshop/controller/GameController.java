a232d7dd7fd9002a2e23e5c021841bfa2b8eeb2f
Béres Dani <beres.dani4@gmail.com>
2022-05-17 16:53:30 +0200

    Additional changes to GameController.

--- src/main/java/hu/nye/progkor/gameshop/controller/GameController.java
+++ src/main/java/hu/nye/progkor/gameshop/controller/GameController.java
@@ -30,7 +30,7 @@ public class GameController {
     /**
      * Lists all the games.
      */
-    @GetMapping
+    @GetMapping("/list.html")
     public String getAllGame(final Model model) {
         final List<Game> games = gameShopService.getAllGames();
         model.addAttribute("games", games);
